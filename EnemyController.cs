14050bbf2fe8814e566e441c8b40de734eaa08b1
Biro Nader <bironader123@gmail.com>
2018-04-28 17:03:49 +0200

    Add Scripts and scene

---
+++ EnemyController.cs
@@ -0,0 +1,168 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	NavMeshAgent nav; 
+	Transform player;
+	Animator controller;
+	GameManagement game;
+	Animator anim;     
+	float health;
+	bool isDead = false;
+	int damage = 20;
+	int pointValue = 10;
+	bool behindWall = true;
+
+	public AudioClip[] attackClips;
+	public AudioClip[] deathClips;
+
+	public float timeToAttack;
+	float attackTimer;
+	public AudioClip zombieAttackClip;
+
+	CapsuleCollider capsuleCollider;
+
+	// Use this for initialization
+	void Awake () {
+		nav = GetComponent <NavMeshAgent> ();
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		controller = GetComponentInParent<Animator> ();
+		game = FindObjectOfType<GameManagement> ();
+		health = 20 + (10 * game.round);
+		capsuleCollider = GetComponent <CapsuleCollider> ();
+		anim = GetComponent <Animator> ();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isDead)
+		{
+			nav.SetDestination (player.position);
+			controller.SetFloat ("speed", Mathf.Abs(nav.velocity.x) + Mathf.Abs (nav.velocity.z));
+			float distance = Vector3.Distance(transform.position, player.position);
+			if(distance < 3  || behindWall)
+			{
+				attackTimer += Time.deltaTime;
+			}
+			else if (attackTimer > 0)
+			{
+				anim.SetBool ("Attack", false);
+				attackTimer -= Time.deltaTime*2;
+			}
+			else
+			{
+				attackTimer = 0;
+				anim.SetBool ("Attack", false);
+			}
+
+		}
+	}
+
+	bool Attack()
+	{
+		if(!anim.GetBool ("Attack"))
+			anim.SetFloat ("AttackType",Random.Range (0,2));
+
+		anim.SetBool ("Attack", true);
+
+		if(!GetComponent<AudioSource>().isPlaying)
+		{
+			//audio.PlayOneShot(attackClips[Random.Range (0,attackClips.Length-1)]);
+			GetComponent<AudioSource>().clip = attackClips[Random.Range (0,attackClips.Length-1)];
+			GetComponent<AudioSource>().Play ();
+		}
+		if(attackTimer > timeToAttack)
+		{
+			attackTimer = 0;
+			return true;
+		}
+		return false;
+	}
+
+
+	void Death ()
+	{
+		// The enemy is dead.
+		isDead = true;
+		nav.Stop ();
+		// Turn the collider into a trigger so shots can pass through it.
+		capsuleCollider.isTrigger = true;
+		capsuleCollider.enabled = false;
+		
+
+		anim.SetTrigger ("Dead");
+
+		GetComponent<AudioSource>().clip = deathClips [Random.Range (0, deathClips.Length-1)];
+		GetComponent<AudioSource>().Play ();
+
+		GameManagement.zombiesLeftInRound -= 1;
+		Destroy (gameObject, 4f);
+	}
+
+
+	void ApplyDamage(float damage)
+	{
+
+		health -= damage;
+
+		if (!isDead)
+		{
+			GameManagement.AddPoints(pointValue);
+			if (health <= 0)
+			{
+				Death ();
+			
+			}
+		}
+
+
+	}
+	void OnCollisionStay(Collision collisionInfo)
+	{
+		if(collisionInfo.gameObject.tag == "Player")
+		{
+			if(Attack ())
+				collisionInfo.collider.SendMessageUpwards("PlayerDamage", damage, SendMessageOptions.RequireReceiver);
+		}
+
+
+	}
+
+	void OnTriggerStay(Collider collide)
+	{
+		//print ("collision");
+		if(collide.gameObject.tag == "SpawnWall")
+		{
+			if(collide.gameObject.GetComponent<Renderer>().enabled)
+			{
+				behindWall = true;
+				nav.Stop ();
+				if(Attack ())
+				{
+					collide.GetComponent<Collider>().SendMessageUpwards("RemoveBoard", SendMessageOptions.RequireReceiver);
+				}
+				
+			}
+			else if(behindWall)
+			{
+				nav.Resume();
+				behindWall = false;
+				anim.SetTrigger("JumpThrough");
+			}
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ GameManagement.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagement : MonoBehaviour {
+	public int round = 1;
+	int zombiesInRound = 10;
+	public static int zombiesLeftInRound = 10;
+	int zombiesSpawnedInRound = 0;
+	float zombieSpawnTimer = 0;
+	public Transform[] zombieSpawnPoints;
+	public GameObject zombieEnemy;
+	public static bool player1HasJug = false;
+
+
+
+	float countdown= 0;
+
+	public static int playerScore = 0;
+	public static int playerCash = 0;
+
+
+	public GUISkin mySkin;
+
+	// Use this for initialization
+	void Awake () {
+		Screen.lockCursor = true;
+		
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(zombiesSpawnedInRound < zombiesInRound  && countdown == 0)
+		{
+			if(zombieSpawnTimer > 2)
+			{
+				SpawnZombie ();
+				zombieSpawnTimer = 0;
+			}
+			else
+			{
+				zombieSpawnTimer+=Time.deltaTime;
+			}
+		}
+		else if (zombiesLeftInRound == 0)
+		{
+			StartNextRound();
+		}
+
+
+		if(countdown > 0)
+			countdown -= Time.deltaTime;
+		else 
+			countdown = 0;
+	}
+
+	public static void AddPoints(int pointValue)
+	{
+		playerScore += pointValue;
+		playerCash += pointValue;
+	}
+
+
+	void SpawnZombie()
+	{
+		Vector3 randomSpawnPoint = zombieSpawnPoints[Random.Range (0,zombieSpawnPoints.Length)].position;
+		Instantiate(zombieEnemy,randomSpawnPoint,Quaternion.identity);
+		zombiesSpawnedInRound ++;
+	}
+
+	void StartNextRound()
+	{
+		zombiesInRound = zombiesLeftInRound = round * 10;
+		zombiesSpawnedInRound = 0;
+		countdown = 15;
+		round++;
+	}
+
+	void OnGUI()
+	{
+		GUI.skin = mySkin;
+		GUIStyle style1 = mySkin.customStyles[0];
+
+		GUI.Label(new Rect(40, Screen.height - 80, 100, 60), " SCORE :");
+		GUI.Label(new Rect(100, Screen.height - 80, 160, 60), "" + playerScore, style1);
+		
+		GUI.Label(new Rect(40, Screen.height - 110, 100, 60), " $ :");
+		GUI.Label(new Rect(100, Screen.height - 110, 160, 60), "" + playerCash, style1);
+
+		GUI.Label(new Rect(540, Screen.height - 80, 100, 60), " Z :");
+		GUI.Label(new Rect(600, Screen.height - 80, 160, 60), "" + zombiesLeftInRound, style1);
+
+		if(countdown != 0)
+		{
+			GUI.Label(new Rect(Screen.width/2 - 50, Screen.height/2 - 80, 100, 60), " NextRound :");
+			GUI.Label(new Rect(Screen.width/2 + 50, Screen.height/2 - 80, 160, 60), "" + Mathf.RoundToInt(countdown), style1);
+		}
+
+		GUI.Label(new Rect(740, Screen.height - 80, 100, 60), " Spawned :");
+		GUI.Label(new Rect(800, Screen.height - 80, 160, 60), "" + zombiesSpawnedInRound, style1);
+		
+	}
+
+}
